f5d739e713a797492fc00c2227ac00bcf872f9f0
Candra Herdiansyah <candraherdiansyah14@gmail.com>
2019-02-01 11:11:51 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,32 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { NgModule, ErrorHandler } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage } from "../pages/about/about";
+import { ContactPage } from "../pages/contact/contact";
+import { HomePage } from "../pages/home/home";
+import { TabsPage } from "../pages/tabs/tabs";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
+// Tambah disini "Saat mengambil API harus ada di mode produksi"
+import { HttpClientModule } from "@angular/common/http";
+import { enableProdMode } from "@angular/core";
+enableProdMode();
+// Akhir tambah disini
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+  // dibagian imports tambah "HttpClientModule"
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(MyApp)],
+
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
+  entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  // Tambah disini
+  public items: any;
+  // Tambah "public http: HttpClient" dibagian constructor
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    //memanggil method getData
+    this.getData();
   }
-
+  // buat method getData()
+  getData() {
+    let url = "http://test.pro/api/latihan";
+    let data: Observable<any> = this.http.get(url);
+    data.subscribe(result => {
+      this.items = result;
+      console.log(result);
+    });
+  }
 }
